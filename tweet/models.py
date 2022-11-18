f768ab201d59ad0cbe4a057af703d0521594198e
sakthivellabglo <sakthivel.labglo@gmail.com>
2022-11-18 15:37:18 +0530

    first commit

--- tweet/models.py
+++ tweet/models.py
@@ -12,10 +12,10 @@ class Tweet(models.Model):
                               related_name='tweets',
                               on_delete=models.CASCADE)
     is_public = models.BooleanField(default=True, null=False, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    modified_at = models.DateTimeField(auto_now=True, editable=False)
-    likes_count = models.IntegerField(default=0, null=False, blank=False, editable=False)
-    comments_count = models.IntegerField(default=0, null=False, blank=False, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    likes_count = models.IntegerField(default=0, null=False, blank=False)
+    comments_count = models.IntegerField(default=0, null=False, blank=False)
 
     def __str__(self):
         return self.text
